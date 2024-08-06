464228dfcf1f391faeda2790247de0166bb2abe9
Matin2009pro <matin.mn.mehnati@gmail.com>
2024-08-06 23:07:55 +0330

    Update text.py

--- bot/homework_handler/text.py
+++ bot/homework_handler/text.py
@@ -1,2 +1,3 @@
 text_receive_homework = "جواب تکلیف شما"
 text_cant_receive_homework = "تکلیف شما به سرور ارسال نشد.\nلطفا دوباره تلاش کنید"
+cant_find_text = "متاسفانه اشکالی در دریافت جواب پیش امده است"
